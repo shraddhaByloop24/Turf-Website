229abac876fb7ed6b2c1f473f7b9eb3b9e1f860c
shraddhabyloop24 <shraddhapatel4829907@gmail.com>
2024-07-23 15:10:12 +0530

    header Complete

--- Turf/src/modules/Header.jsx
+++ Turf/src/modules/Header.jsx
@@ -6,19 +6,19 @@ const Header = ({ phone, email }) => {
     <>
     <header className='main-header w-100 '>
         <div className="container-fluid">
-        <div className="row w-100">
-            <div className="col-lg-6 header-1 px-4">
+        <div className="row d-flex justify-content-between w-100">
+            <div className="col-lg-6 header-1 px-lg-4">
                 <div className='header-left d-flex'>
-                    <div className='header-phone '><i className="fa-solid fa-phone-volume"></i><a href={phone}>0123-452-789</a></div>
-                    <div className='header-email'><i class="fa-solid fa-envelope"></i><a href={email}>Hello@byloop.in</a></div>
+                    <div className='header-phone '><i className="fa-solid fa-phone-volume px-2"></i><a href={phone}>0123-452-789</a></div>
+                    <div className='header-email'><i className="fa-solid fa-envelope px-2"></i><a href={email}>Hello@byloop.in</a></div>
                 </div>
             </div>
             <div className="col-lg-6 ">
-                <div className='header-right bg-danger'>
-                    <p><a><i className="fa-brands fa-square-instagram"></i></a></p>
-                    <p><a><i className="fa-brands fa-square-facebook"></i></a></p>
-                    <p><a><i className="fa-brands fa-linkedin"></i></a></p>
-                    <p><a><i className="fa-brands fa-square-twitter"></i></a></p>
+                <div className='header-right pt-lg-1 pb-lg-0 pb-2'>
+                    <a><i className="fa-brands fa-square-instagram"></i></a>
+                    <a><i className="fa-brands fa-square-facebook"></i></a>
+                    <a><i className="fa-brands fa-linkedin"></i></a>
+                    <a><i className="fa-brands fa-square-twitter"></i></a>
                 </div>
             </div>
         </div>
